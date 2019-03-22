f2a9b0aa50db79b18206a7c79e38d12e4081301b
yangdepu <yangdepu@zdsoft.cn>
2019-03-22 15:50:50 +0800

    两数之和

--- 1.两数之和.js
+++ 1.两数之和.js
@@ -33,13 +33,69 @@ var twoSum = function (nums, target) {
   if (nums.length <= 1) {
     return false;
   }
-  let len = nums.length;
-  let obj = {};
-  for (let i = 0; i < len; i++) {
-    if (nums[i] in obj) {
-      return [obj[nums[i]], i]
+  //  answer1 暴力双层循环
+  // let arr1 = [];
+  // for (let i1 = 0; i1 < nums.length; i1++) {
+  //   for (let j1 = i1 + 1; j1 < nums.length; j1++) {
+  //     if (nums[i1] + nums[j1] === target) {
+  //       arr1 = [i1, j1];
+  //       break;
+  //     }
+  //   }
+  // }
+  // return arr1
+
+  //  --------------------------------------------------
+
+  //  answer2 用indexOf优化代码
+  // let a, b;
+  // for (let i2 = 0; i2 < nums.length; i2++) {
+  //   b = nums.indexOf(target - nums[i2]);
+  //   if (b != -1 && b != i2) {
+  //     a = i2
+  //     break;
+  //   }
+  // }
+  // return [a, b]
+
+  //  --------------------------------------------------
+
+  //  answer3用对象
+  // let temObj = {};
+  // nums.forEach((item, index) => {
+  //   temObj[item] = index;
+  // })
+
+  // for (let i = 0; i < nums.length; i++) {
+  //   let j = temObj[target - nums[i]]
+  //   if (j && j != i) {
+  //     return [i, j]
+  //   }
+  // }
+
+  //  --------------------------------------------------
+
+  //answer4用对象的简洁写法
+  // let len = nums.length;
+  // let obj = {};
+  // for (let i = 0; i < len; i++) {
+  //   if (nums[i] in obj) {
+  //     return [obj[nums[i]], i]
+  //   } else {
+  //     obj[target - nums[i]] = i;
+  //   }
+  // }
+
+  //  --------------------------------------------------
+
+  //  用map
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      return [map.get(nums[i]), i]
     } else {
-      obj[target - nums[i]] = i;
+      map.set((target - nums[i]), i);
     }
   }
+
 };
