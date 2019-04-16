568b9af200f4db494eb23b99d64928b47a1bb19d
yangdepu <yangdepu@zdsoft.cn>
2019-04-16 16:53:01 +0800

    some fix

--- 1.两数之和.java
+++ 1.两数之和.java
@@ -25,20 +25,20 @@
  * 
  */
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] rst = new int[2];
-        if (nums == null || nums.length <= 1) return rst;
+  public int[] twoSum(int[] nums, int target) {
+    int[] rst = new int[2];
+    if (nums == null || nums.length <= 1)
+      return rst;
 
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(target - nums[i])) {
-                rst[0] = map.get(target - nums[i]);
-                rst[1] = i;
-                break;
-            } 
-            map.put(nums[i], i);
-        }
-        return rst;
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
+      if (map.containsKey(target - nums[i])) {
+        rst[0] = map.get(target - nums[i]);
+        rst[1] = i;
+        break;
+      }
+      map.put(nums[i], i);
     }
+    return rst;
+  }
 }
-
